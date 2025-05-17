7855b4f75b3b807a7b9c37baa36771fe8698feca
Miracle-Lawrence <youngmiracle01@gmail.com>
2025-05-17 08:00:33 +0100

    event listener completed

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,29 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-list.appendChild(li);
 
+
+const para = document.createElement("p");
+// for the buttons 
+button.addEventListener("click", () => {
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener("click", () => {
+            list.removeChild(li);
+            input.focus();
+        });
+        
+    input.value = "";
+    input.focus();
+    }
+});
+    
 
 
